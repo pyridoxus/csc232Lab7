c8ef37ae0aebbc3715d673ab004f9a83ba67cf95
Craig McCulloch <pyridoxus@null.net>
2012-08-26 11:22:56 -0500

    Added GNU GPL to files.

--- lab7.cpp
+++ lab7.cpp
@@ -1,9 +1,15 @@
-/*
- * lab7.cpp
- *
- *  Created on: Apr 8, 2012
- *      Author: pyridoxus
- */
+//This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "lab7.h"
 
 int main( int argc, char *argv[] )
